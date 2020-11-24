783c8b49e5e0de026b54e0a112662fd8495125f1
Angel Vargas <angelvargas@outlook.es>
2020-11-24 20:35:52 +0000

    k100: naive word wrap

--- katas/k100/main_test.go
+++ katas/k100/main_test.go
@@ -6,18 +6,20 @@ import (
 
 func TestWordWrap(t *testing.T) {
 	var tests = []struct {
-		words string
-		lines_expected int
-
+		words         string
+		linesExpected int
 	}{
-		{"this", 1},
-		{"this do that", 3},
-		{"if this is longer than 3 then 8", 8}
+		{"foo", 1},
+		{"foo do bar", 3},
+		{"if this is longer than 3 then 8", 8},
+		{"if this is  double spaces we want  7", 8},
+		{"", 0},
+		{"  test  ", 1},
 	}
 
 	for _, test := range tests {
-		if output := WordWrap(test.words); output != test.lines_expected  {
-			t.Error("Test Failed: {} inputted, {} expected, received {}", test.input, test.lines_expected, output)
+		if _, output := WordWrap(test.words); output != test.linesExpected {
+			t.Error("Test Failed: {} inputted, {} expected, received {}", test.words, test.linesExpected, output)
 		}
 	}
 }
